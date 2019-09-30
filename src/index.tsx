dec1331066049125ab8d9bcc318fac4ec3317709
anton-voron <anton.voron.gt@gmail.com>
2019-09-30 15:47:49 +0300

    Workable project

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.sass';
 import * as serviceWorker from './serviceWorker';
 
+import { observable, action } from 'mobx'
+import { observer } from 'mobx-react'
 
+class InputState {
 
-class App extends Component {
-    render(){
-        return(
-            <h1 className="title">Hello task!</h1>
-        )
+    @observable
+    value: number = 0
+    
+     @action
+     onChange = (): void => {
+         console.log(this.value)
+         this.value++
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const props = new InputState()
+
+
+interface Iprops {
+    InputState: InputState
+}
+
+
+@observer
+class App extends Component<Iprops> {
+
+    handleInc = (): void => {
+        this.props.InputState.onChange()
+    }
+    render(){
+        return<>
+            <h1 className="title">{this.props.InputState.value}</h1>
+            <button onClick={this.handleInc}>+1</button>
+        </>
+    }
+}
+
+ReactDOM.render(<App InputState={props}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
